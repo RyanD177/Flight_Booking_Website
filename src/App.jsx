0dd7ebca15a8861bdb92b022847681c0bbd61d86
RyanD177 <w0477770@campus.nscc.ca>
2023-12-31 08:05:02 -0400

    header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import "./App.css";
 import Header from "./components/Header";
+
+
+
 function App() {
   return (
-    <>
+    <div className="bg-[url(../src/assets/banner.jpg)]">
       <Header />
       <section>
         <div>Booking</div>
         <div>Table</div>
       </section>
-    </>
+    </div>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
-
+import { MdFlight } from "react-icons/md";
 
 const Header = () => {
   return (
 
       <header className="w-full px-4 bg-slate-50"> 
-        <nav className="flex item-center justify-between max-w-6xl">
-            <a href="/" className="text-lg font-bold ">Travel Logo</a>
+        <nav className="flex item-center justify-between max-w-6xl mx-auto">
+            <a href="/" className="text-lg font-bold flex items-center" > Travel Logo<MdFlight className="text-2xl mr-1 text-indigo-600"/></a>
             <button className="bg-indigo-500 text-white px-6 py-2 rounded font-medium">Login</button>
         </nav>
       </header>
